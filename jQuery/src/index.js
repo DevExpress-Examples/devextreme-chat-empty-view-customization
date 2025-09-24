27d061a969551b50535439bb9a9e6135ad558ebd
Arman Boyakhchyan <arman.boyakhchyan@devexpress.com>
2025-09-24 11:48:23 +0400

    Add jQuery & remove ASP.NET Core

--- jQuery/src/index.js
+++ jQuery/src/index.js
@@ -1,10 +1,52 @@
+const tiles = [{
+  emoji: '🗓️',
+  text: 'View Available Vacation Days',
+}, {
+  emoji: '🎉',
+  text: 'List Upcoming Holidays',
+}, {
+  emoji: '📄',
+  text: 'Submit a Vacation Request',
+}];
+
 $(() => {
-  let count = 0;
-  $('#btn').dxButton({
-    text: `Click count: ${count}`,
-    onClick(e) {
-      count += 1;
-      e.component.option('text', `Click count: ${count}`);
+  $('#chat').dxChat({
+    width: 780,
+    height: 480,
+    emptyViewTemplate: (data) => {
+      const $container = $('<div>').addClass('empty-view-container').css({ 'width': '100%', 'height': '100%' });
+      $container.append(
+        $('<div>').addClass('empty-view-titlebox').append(
+          $('<div>').addClass('empty-view-title').text('How can HR Assistant help you today?'),
+          $('<div>').addClass('empty-view-subtitle').text(data.texts.message),
+        ),
+        $('<div>').attr('id', 'tile-view-container').dxTileView({
+          width: 728,
+          height: 124,
+          baseItemWidth: 226,
+          baseItemHeight: 96,
+          itemMargin: 12,
+          dataSource: tiles,
+          activeStateEnabled: false,
+          onItemClick(event) {
+            DevExpress.ui.notify(`You clicked '${event.itemData.text}'.`);
+          },
+          itemTemplate(itemData) {
+            const $itemContainer = $('<div>').addClass('tile-container');
+            $itemContainer.append(
+              $('<div>').addClass('tile-emoji').text(itemData.emoji),
+              $('<div>').addClass('tile-text').text(itemData.text),
+            );
+
+            return $itemContainer;
+          },
+        }),
+      );
+
+      return $container;
+    },
+    onMessageEntered: ({ component, message }) => {
+      component.renderMessage(message);
     },
   });
 });
