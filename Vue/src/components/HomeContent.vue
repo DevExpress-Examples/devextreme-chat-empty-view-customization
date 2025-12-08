d4ac027dbaaa762aa5c1118f849c2495c2a022c1
Arman Boyakhchyan <arman.boyakhchyan@devexpress.com>
2025-12-08 13:23:07 +0400

    Add Final Updates

--- Vue/src/components/HomeContent.vue
+++ Vue/src/components/HomeContent.vue
@@ -1,7 +1,6 @@
 <script setup lang="ts">
-import { ref, type Ref } from 'vue';
 import 'devextreme/dist/css/dx.fluent.blue.light.css';
-import { DxChat, type DxChatTypes } from 'devextreme-vue/chat';
+import { DxChat } from 'devextreme-vue/chat';
 import { DxTileView } from 'devextreme-vue/tile-view';
 
 const tiles = [{
@@ -14,45 +13,39 @@ const tiles = [{
   emoji: '📄',
   text: 'Submit a Vacation Request',
 }];
-
-const messages: Ref<DxChatTypes.Message[]> = ref([]);
-
-function onMessageEntered(e: DxChatTypes.MessageEnteredEvent): void {
-  messages.value = [...messages.value, e.message];
-}
 </script>
 <template>
   <DxChat
     id="chat"
     :width="780"
-    :height="480"
-    :items="messages"
-    @message-entered="onMessageEntered"
+    :height="520"
     empty-view-template="emptyView"
   >
     <template #emptyView="{ data }">
-      <div class="empty-view-titlebox">
-        <div class="empty-view-title">How can HR Assistant help you today?</div>
-        <div class="empty-view-subtitle">{{ data.texts.message }}</div>
+      <div class="empty-view-container">
+        <div class="empty-view-titlebox">
+          <div class="empty-view-title">How can HR Assistant help you today?</div>
+          <div class="empty-view-subtitle">{{ data.texts.message }}</div>
+        </div>
+        <DxTileView
+          id="tile-view-container"
+          :data-source="tiles"
+          :width="728"
+          :height="124"
+          :base-item-width="226"
+          :base-item-height="96"
+          :item-margin="12"
+          :active-state-enabled="false"
+          item-template="itemTemplate"
+        >
+          <template #itemTemplate="{ data }">
+            <div class="tile-container">
+              <div class="tile-emoji">{{ data.emoji }}</div>
+              <div class="tile-text">{{ data.text }}</div>
+            </div>
+          </template>
+        </DxTileView>
       </div>
-      <DxTileView
-        id="tile-view-container"
-        :data-source="tiles"
-        :width="728"
-        :height="124"
-        :base-item-width="226"
-        :base-item-height="96"
-        :item-margin="12"
-        :active-state-enabled="false"
-        item-template="itemTemplate"
-      >
-        <template #itemTemplate="{ data }">
-          <div class="tile-container">
-            <div class="tile-emoji">{{ data.emoji }}</div>
-            <div class="tile-text">{{ data.text }}</div>
-          </div>
-        </template>
-      </DxTileView>
     </template>
   </DxChat>
 </template>
