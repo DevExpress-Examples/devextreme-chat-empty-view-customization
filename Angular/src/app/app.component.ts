25ffc569e205b7f75039b8f3b40983b8a47efede
AlisherAmonulloev <AlisherAmonulloev@users.noreply.github.com>
2026-05-27 13:55:06 +0300

    Migrate Angular: update to v21, standalone components, flat ESLint config

--- Angular/src/app/app.component.ts
+++ Angular/src/app/app.component.ts
@@ -1,7 +1,10 @@
 import { Component } from '@angular/core';
+import { DxChatModule } from 'devextreme-angular/ui/chat';
+import { DxTileViewModule } from 'devextreme-angular/ui/tile-view';
 
 @Component({
   selector: 'app-root',
+  imports: [DxChatModule, DxTileViewModule],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
